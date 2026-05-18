b97d94eeb1ee1eb1fd927734310bed19bdb15e60
Eldissone Vilonga <Evilonga@Uni01.local>
2026-05-18 11:07:14 +0100

    up

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  preview: {
+    allowedHosts: ['eldissone.onrender.com']
+  },
   build: {
     rollupOptions: {
       input: {
