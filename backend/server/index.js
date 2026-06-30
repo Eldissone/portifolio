51913c0f28d79cc91d7408cdd3eec785cde1b470
Eldissone <eldissonev@gmail.com>
2026-06-30 20:51:33 +0100

    up

--- backend/server/index.js
+++ backend/server/index.js
@@ -34,6 +34,7 @@ const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:4173',
   'https://www.eldissone.com',
+  'https://eldissone.com',
   process.env.FRONTEND_URL
 ].filter(Boolean);
 
